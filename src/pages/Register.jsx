bba65152674f5d51a0f4310c9ea2ca2c4d8a1c65
john2026mainakasongo-bit <john2026mainakasongo@gmail.com>
2026-07-05 06:32:29 -0700

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,1 +1,20 @@
+export default function Register({ goLogin, onRegister }) {
+  function register() {
+    localStorage.setItem("token", "demo-token");
+    onRegister();
+  }
 
+  return (
+    <div className="authPage">
+      <div className="authBox">
+        <h1>MetaBinary</h1>
+        <h2>Create Account</h2>
+        <input placeholder="Full name" />
+        <input placeholder="Email" />
+        <input placeholder="Password" type="password" />
+        <button onClick={register}>Register</button>
+        <p onClick={goLogin}>Already have an account?</p>
+      </div>
+    </div>
+  );
+}
