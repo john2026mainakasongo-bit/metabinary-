b4460c7194553f5198be330a0cbb3439cd97f8f0
john2026mainakasongo-bit <john2026mainakasongo@gmail.com>
2026-07-05 07:02:54 -0700

    Update TradePanel.jsx

--- src/components/TradePanel.jsx
+++ src/components/TradePanel.jsx
@@ -1,1 +1,130 @@
 
+export default function TradePanel(){
+
+return(
+
+<div className="tradePanel">
+
+<p className="learn">
+
+ⓘ Learn about this trade type
+
+</p>
+
+<h1 className="tradeTitle">
+
+Even/Odd
+
+</h1>
+
+<div className="contractTabs">
+
+<button className="active">
+
+Even/Odd
+
+</button>
+
+<button>
+
+Matches/Differs
+
+</button>
+
+<button>
+
+Over/Under
+
+</button>
+
+<button>
+
+Rise/Fall
+
+</button>
+
+<button>
+
+Touch/No Touch
+
+</button>
+
+</div>
+
+<div className="tradeMode">
+
+<span>
+
+Trade Mode
+
+</span>
+
+<b>
+
+Manual
+
+</b>
+
+</div>
+
+<div className="choice">
+
+<button className="green">
+
+Even
+
+</button>
+
+<button className="dark">
+
+Odd
+
+</button>
+
+</div>
+
+<label>
+
+Duration ticks
+
+</label>
+
+<input value="5"/>
+
+<label>
+
+Stake
+
+</label>
+
+<input value="10"/>
+
+<div className="buyEven">
+
+Even
+
+<span>
+
+Payout 19 USD
+
+</span>
+
+</div>
+
+<div className="buyOdd">
+
+Odd
+
+<span>
+
+Payout 19 USD
+
+</span>
+
+</div>
+
+</div>
+
+)
+
+}
